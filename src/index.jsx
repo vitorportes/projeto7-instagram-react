a596f81bbd52c44efa7bca78a4be60024e18e9a0
vitorportes <vitorportnoy@gmail.com>
2022-03-14 16:19:07 -0300

    content not showing fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import reactDom from "react-dom";
+import reactDOM from "react-dom";
 import Navbar from "./Navbar";
 import Corpo from "./Corpo";
 import FundoMobile from "./FundoMobile";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Corpo />
       <FundoMobile />
-    </div>
+    </>
   );
 }
 
-reactDom.render(<App />, document.querySelector(".root"));
+reactDOM.render(<App />, document.querySelector(".root"));
